11a412126ef6cfece030c2f248e5a4736937c5b1
Devin Riley <devin.riley144@gmail.com>
2023-01-27 09:58:22 -0600

    finished week 3 chal

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,147 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            var alpha = "abcdefghijklmnopqrstuvwxyz";
+            return alpha.Contains(char.ToLower(c));
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if(vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-
+         // return vals.Length % 2 == 0; <----  you could also write it like this 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return (num % 2 != 0);
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            double min = 0;
+            double max = 0;
+
+            foreach(var num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+
+                if (numbers.Count() <= 1)
+                {
+                    min = num;
+                }
+            }
+            return min + max;
+            // could also be written as ---> return numbers?.Any() ??false ? numbers.Min() + numbers.Max() : new double {};
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
+            //could also be written as ----> return (str1.Length < str2.Length) ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+
+            for(var i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+            //could also be written as ----> return (numbers == null) ? 0 : numbers.Sum();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            var evenSum = 0;
+
+            for (int i = 0; i < numbers.Length;i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+            }
+            return evenSum;
+            //could also be written as -----> return numbers?.Where(x => x % 2 == 0).Sum() ?? 0;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null )
+            {
+                return false;
+            }
+
+            var sum = 0;
+
+            for(int i = 0; i < numbers.Count ; i++)
+            {
+                sum += numbers[i];
+            }
+            return (sum %2 != 0);
+            //could also be written as ----> return (numbers == null) ? false : numbers.Sum() % 2 != 0
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
+            //could also be written as -----> retrurn(number <=0) ? 0 : number / 2; 
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,121 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for(var i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+            //throw new NotImplementedException();
+            //way to do it using LINQ vvvvv
+            //return vals.Contains(false);
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            //if (numbers == null)
+            //{
+            //    return false;
+            //}
+
+
+            //var odds = new List<int>();
+            //foreach (var num in numbers) 
+            //{
+            //    if(num %2 != 0)
+            //    {
+            //        odds.Add(num);
+            //    }
+            //}
+
+            //var sumOdds = odds.Sum();
+
+            //return (sumOdds % 2 != 0);
+
+            return (numbers == null) ? false : (numbers.Where(x => x % 2 != 0).Sum() % 2 != 0);
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
+
+            for(var i = 0; i < password.Length;i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower= true;
+                }
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper= true;
+                }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber= true;
+                }             
+            }
+
+            if (isLower == true && isUpper == true && isNumber == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor== 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+                for(var i = 0; i <words.Length;i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
